d429b9be4329b11b8e818c05354f6e69fdda294a
Soorya S <suri111200@gmail.com>
2019-03-19 12:21:31 -0400

    Actual stuff

--- a3_300069290_300065226/01/GameModel.java
+++ a3_300069290_300065226/01/GameModel.java
@@ -2,4 +2,23 @@ public class GameModel {
 
  // Your code here
 
+	private int height;
+	private int width;
+
+	public GameModel(int width, int height){
+		this.width = width;
+		this.height = height;
+	}
+
+	public int getHeight(){
+		
+		return height;
+	}
+
+	public int getWidth(){
+
+		return height;
+
+	}
+
 }
