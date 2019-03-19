5db1e7eb22adaaf5259917710ad63eba8ffccd59
Soorya S <suri111200@gmail.com>
2019-03-19 12:49:14 -0400

    Finished 01/GameModel.java

--- a3_300069290_300065226/01/GameModel.java
+++ a3_300069290_300065226/01/GameModel.java
@@ -16,7 +16,7 @@ public class GameModel {
 		this.width = width;
 		this.height = height;
 
-		gameBoard = new boolean[width][height]
+		gameBoard = new boolean[height][width]
 	}
 
 	public int getHeight(){
@@ -26,13 +26,40 @@ public class GameModel {
 
 	public int getWidth(){
 
-		return height;
+		return width;
 
 	}
 
 	public boolean isON(int i, int j){
 
-		return gameBoard[i][j];
+		return gameBoard[j][i];
 	}
 
+	public void reset(){
+		gameBoard = new boolean[width][height];
+	}
+
+	public void set(int i, int j, boolean value){
+		gameBoard[i][j] = value;
+	}
+
+	public String toString(){
+
+		String toreturn = "[[";
+        for (int i = 0; i < gameBoard.length; i++)
+        {
+            for (int j = 0; j < gameBoard[i].length; j++)
+            {
+                toreturn += gameBoard[i][j];
+               // toreturn += selHolder[i][j];
+                if (j != gameBoard[i].length-1)
+                    toreturn += ", ";
+            } 
+            if (i != gameBoard.length-1)
+                toreturn += "],\n";
+        }
+        toreturn += "]]";
+
+        return toreturn;
+	}
 }
