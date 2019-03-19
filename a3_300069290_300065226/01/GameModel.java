730147cd42e83856bad0595a290007ba86174b5d
Soorya S <suri111200@gmail.com>
2019-03-19 12:33:07 -0400

    Actual stuff

--- a3_300069290_300065226/01/GameModel.java
+++ a3_300069290_300065226/01/GameModel.java
@@ -5,9 +5,13 @@ public class GameModel {
 	private int height;
 	private int width;
 
+	private boolean[][]gameBoard;
+
 	public GameModel(int width, int height){
 		this.width = width;
 		this.height = height;
+
+		gameBoard = new boolean[width][height]
 	}
 
 	public int getHeight(){
@@ -19,6 +23,11 @@ public class GameModel {
 
 		return height;
 
+	}
+
+	public boolean isON(int i, int j){
+
+		return gameBoard[i][j];
 	}
 
 }
